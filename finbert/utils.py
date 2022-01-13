f914832520ea15dc25cd59dbaa8bf47eb47f8731
Ari Shater <ashater@gmail.com>
2022-01-12 20:33:59 -0500

    removing forced log config

--- finbert/utils.py
+++ finbert/utils.py
@@ -13,9 +13,9 @@ import torch
 import numpy as np
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt='%m/%d/%Y %H:%M:%S',
-                    level=logging.INFO)
+# logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+#                     datefmt='%m/%d/%Y %H:%M:%S',
+#                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
